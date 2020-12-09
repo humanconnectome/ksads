bd8c7f86071aad24a04774849a7c604ee1daf41a
Moises Baltazar <me@moisesb.com>
2020-12-09 10:34:39 -0600

    Add a function that converts columns to numeric

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from utils import utils
 import pandas as pd
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
 import pytest
 
 
@@ -18,3 +19,44 @@ def test_strip_special_chars():
     )
     actual = utils.strip_special_chars(input_)
     assert actual.equals(expected)
+
+
+def test_to_numeric_float():
+    input_ = pd.Series(["1.0", "2", 3.0, ""])
+    actual = utils.to_numeric(input_)
+    expected = pd.Series([1, 2, 3, None], dtype=float)
+
+    assert input_.dtype == "O"
+    assert actual.dtype == float
+    assert actual.equals(expected)
+
+
+def test_to_numeric_str():
+    input_ = pd.Series(["1.0", "2", 3.0, "Blah"])
+    actual = utils.to_numeric(input_)
+    expected = pd.Series(["1.0", "2", 3.0, "Blah"])
+
+    assert input_.dtype == "O"
+    assert actual.dtype == "O"
+    assert actual.equals(expected)
+
+
+def test_to_numeric_multiple_columns():
+    input_ = pd.DataFrame([[0, 1, 2], [2.0, 2.0, ""]])
+    actual = input_.apply(utils.to_numeric)
+    expected_dtypes = [int, int, float]
+
+    assert (actual.dtypes == expected_dtypes).all()
+
+
+def test_to_numeric_multiple_date_types():
+    input_ = pd.DataFrame(
+        [
+            pd.to_datetime(["2020-02-01", "2020-02-01"]),
+            pd.to_datetime(["2020-02-01", "2020-02-01 01:25:44 PM"]),
+        ]
+    )
+    actual = input_.apply(utils.to_numeric)
+
+    assert input_.dtypes.map(is_datetime).all()
+    assert actual.dtypes.map(is_datetime).all()
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def strip_special_chars(df):
     return df.apply(
         lambda col: col.astype(str)
@@ -8,3 +11,16 @@ def strip_special_chars(df):
         if col.dtype == "O"
         else col
     )
+
+
+def to_numeric(column):
+    # if date, don't convert
+    if column.dtype == "<M8[ns]":
+        return column
+    try:
+        n = pd.to_numeric(column)
+        if n.dtype == float and (n % 1 == 0).all():
+            n = n.astype(int)
+        return n
+    except ValueError:
+        return column
